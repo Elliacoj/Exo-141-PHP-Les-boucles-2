677535a9e1dded6142683bbf4b878824c7e96f62
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-01-22 14:30:28 +0100

    Modif

--- index.php
+++ index.php
@@ -15,4 +15,29 @@ while($y < 21) {
     }
     $y++;
 }
+
+$a = 1;
+$midA = $a / 2;
+
+while($a <= 10) {
+    echo $a . '<br>';
+
+    $a+= $midA;
+}
+
+for($x = 1; $x < 15; $x++) {
+    echo "On y est presque" . '<br>';
+}
+
+for($x = 20; $x > 0; $x--) {
+    echo "C'est presque bon" . '<br>';
+}
+
+for($x = 1; $x < 100; $x+=15) {
+    echo "On tient le bon bout" . '<br>';
+}
+
+for($x = 200; $x > 0; $x-=12) {
+    echo "Enfin" . '<br>';
+}
 ?>
